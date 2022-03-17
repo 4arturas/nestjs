3872dfac35064d934d6d29fa58d94afe8d467797
Arturas Dirvonskis <Arturas.Dirvonskis@probanx.com>
2022-03-17 07:35:25 +0200

    auth2

--- server/src/tasks/tasks.repository.ts
+++ server/src/tasks/tasks.repository.ts
@@ -3,19 +3,22 @@ import {Task} from "./task.entity";
 import {CreateTaskDto} from "./dto/create-task.dto";
 import {TaskStatus} from "./task-status.enum";
 import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
+import {User} from "../auth/user.entity";
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
 
-    async getTasks(filterDta: GetTasksFilterDto): Promise<Task[]> {
+    async getTasks(filterDta: GetTasksFilterDto, user: User): Promise<Task[]> {
         const {status, search} = filterDta;
         const query = this.createQueryBuilder('task');
+        query.where({user});
+
         if (status) {
             query.andWhere('task.status = :status', { status });
         }
         if (search) {
             query.andWhere(
-                'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+                '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
                 { search: `%${search}%`}
             );
         }
@@ -23,9 +26,14 @@ export class TasksRepository extends Repository<Task> {
         return tasks;
     }
 
-    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
         const {title, description} = createTaskDto;
-        const task = this.create({title, description, status: TaskStatus.OPEN});
+        const task = this.create({
+            title,
+            description,
+            status: TaskStatus.OPEN,
+            user
+        });
         await this.save(task);
         return task;
     }
